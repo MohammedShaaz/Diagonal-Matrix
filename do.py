fbfb5c98ec1f9cdbd996e28d1787bd5c567678d7
MohammedShaaz <84967353+MohammedShaaz@users.noreply.github.com>
2022-10-15 13:23:58 +0530

    Add files via upload

---
+++ do.py
@@ -0,0 +1,30 @@
+row = 3
+col = 4
+tot = row * col
+list = []
+num = 0
+diff = num + 1
+for i in range(row):
+    ele = []
+    for j in range(col):
+            if j == 0:
+                num = num + diff
+                ele.append(num)
+            elif i+ 1 == row and j+ 1 == col:
+                ele.append(tot)
+            else:
+                if diff < row:
+                    diff += 1
+                    num = num + diff + i
+                    ele.append(num)
+                else:
+                    num = num + diff
+                    ele.append(num)
+
+    num = i
+    list.append(ele)
+
+for i in range(row):
+    for j in range(col):
+        print(list[i][j],end="\t")
+    print()
