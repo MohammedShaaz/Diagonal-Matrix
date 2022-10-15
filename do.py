47fa27d6f863494e6902607eb8b6804700463888
MohammedShaaz <84967353+MohammedShaaz@users.noreply.github.com>
2022-10-15 16:27:45 +0530

    Update do.py

--- do.py
+++ do.py
@@ -1,4 +1,4 @@
-row = 3
+row = 4
 col = 4
 tot = row * col
 list = []
@@ -7,21 +7,22 @@ diff = num + 1
 for i in range(row):
     ele = []
     for j in range(col):
-            if j == 0:
+        if  j == 0:
+            num = i + diff
+            ele.append(num)
+        elif i+ 1 == row and j+ 1 == col:
+            ele.append(tot)
+        else:
+            if diff < row:
+                diff += 1
+                num = num + diff + i
+                ele.append(num)
+            else:
                 num = num + diff
                 ele.append(num)
-            elif i+ 1 == row and j+ 1 == col:
-                ele.append(tot)
-            else:
-                if diff < row:
-                    diff += 1
-                    num = num + diff + i
-                    ele.append(num)
-                else:
-                    num = num + diff
-                    ele.append(num)
-
-    num = i
+    diff = i
+    diff += 1
+    num += i
     list.append(ele)
 
 for i in range(row):
